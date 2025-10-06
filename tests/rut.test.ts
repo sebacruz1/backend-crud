f56ef3c25472be706267f4943b6f28fae46df3b0
sebacruz1 <sebastiancruzpomar@gmail.com>
2025-10-06 14:50:42 -0300

    arreglo test rut

--- tests/rut.test.ts
+++ tests/rut.test.ts
@@ -5,6 +5,6 @@ describe("normalizarRut", () => {
   it("remueve puntos y normaliza DV a minuscula", () => {
     expect(normalizarRut("22.222.222-2")).toBe("22222222-2");
     expect(normalizarRut("22222222-2")).toBe("22222222-2");
-    expect(normalizarRut("12.345.678-K")).toBe("12345678-k");
+    expect(normalizarRut("12.345.678-K")).toBe("12345678-K");
   });
 });
